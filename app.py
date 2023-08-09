0b29b6ef2ac3618ceab1eb5db9490e983f7eab52
Kaushik Narasimhan <kaushik.beastmode@gmail.com>
2023-08-09 10:21:21 -0400

    works ig

--- app.py
+++ app.py
@@ -9,6 +9,13 @@ def read_flag_from_file():
     except FileNotFoundError:
         return ''
 
+def calculate_expression(expression):
+    try:
+        result = eval(expression)
+        return str(result)
+    except Exception as e:
+        return str(e)
+
 @app.route('/')
 def index():
     return render_template_string(open('index.html').read())
@@ -16,11 +23,13 @@ def index():
 @app.route('/search')
 def search():
     search_query = request.args.get('query', '')
+    
     if search_query == '{{flag}}':
         flag = read_flag_from_file()
         return render_template_string("Congratulations! Flag: {{ flag }}", flag=flag)
     else:
-        return "Nothing to see here."
+        result = calculate_expression(search_query)
+        return render_template_string(open('index.html').read(), query_result=result, query=search_query)
 
 if __name__ == '__main__':
     app.run(debug=True)
