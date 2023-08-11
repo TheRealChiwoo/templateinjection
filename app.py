cea663193cdac48969579d785facf1363da36e8a
TheRealChiwoo <prozexal@gmail.com>
2023-08-10 22:09:44 -0400

    New Changes

--- app.py
+++ app.py
@@ -1,35 +1,22 @@
+import json
 from flask import Flask, render_template_string, request
 
 app = Flask(__name__)
 
-def read_flag_from_file():
-    try:
-        with open('flag.txt', 'r') as file:
-            return file.read().strip()
-    except FileNotFoundError:
-        return ''
+# Load blog posts from the JSON file
+with open("posts.json") as f:
+    blog_posts = json.load(f)
 
-def calculate_expression(expression):
-    try:
-        result = eval(expression)
-        return str(result)
-    except Exception as e:
-        return str(e)
-
+# Route to render the index.html template
 @app.route('/')
 def index():
-    return render_template_string(open('index.html').read())
+    query = request.args.get('query', '')
 
-@app.route('/search')
-def search():
-    search_query = request.args.get('query', '')
-    
-    if search_query == '{{flag}}':
-        flag = read_flag_from_file()
-        return render_template_string("Congratulations! Flag: {{ flag }}", flag=flag)
-    else:
-        result = calculate_expression(search_query)
-        return render_template_string(open('index.html').read(), query_result=result, query=search_query)
+    # Filter blog posts based on the search query
+    matching_posts = [post for post in blog_posts if query.lower() in (post["title"] + post["text"]).lower()]
 
-if __name__ == '__main__':
-    app.run(debug=True, port=8000)
+    return render_template_string(open('index.html').read(), blog_posts=matching_posts, query_result=query)
+
+if __name__ == "__main__":
+    app.run(port=8000, debug=True, host="0.0.0.0")
+
